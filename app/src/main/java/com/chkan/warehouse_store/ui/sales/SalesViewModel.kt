ead97802630d767082735a77618d2bc06689b64b
git-cdv <chkan.dv@gmail.com>
2021-08-22 13:17:26 +0300

    + Coroutines

--- app/src/main/java/com/chkan/warehouse_store/ui/sales/SalesViewModel.kt
+++ app/src/main/java/com/chkan/warehouse_store/ui/sales/SalesViewModel.kt
@@ -9,6 +9,7 @@ import com.chkan.warehouse_store.utils.Constans
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.*
 import org.joda.time.LocalDate
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.format.DateTimeFormatter
@@ -46,8 +47,8 @@ class SalesViewModel : ViewModel() {
                     var sale = data.getValue(Product::class.java)
                     list.add(sale as Product)
                 }
-                //сортируем по названию сумок
-                _sales.value = list.sortedBy { it.name }
+                //сортируем по названию сумок и фильтрует по остаткам
+                _sales.value = list.sortedBy { it.name }.filter { it.quantity != 0 }
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
@@ -64,25 +65,30 @@ class SalesViewModel : ViewModel() {
 
     fun onReturn() {
         Log.d(Constans.TAG, "SalesViewModel -> onReturn")
+        //создаем неподвязанный скоуп чтобы корутина доработала и после закрытия фрагмента
+        //выбираем Dispatchers.IO потому что запросы в сеть
+        val scope = CoroutineScope(Job() + Dispatchers.IO)
+        scope.launch {
+            // добавляем в остатках
+            //пытаюсь получить значение остатка по этому товару
+            database.child(Constans.KEY_DB_PRODUCTS).child(clickedId.value.toString()).child("quantity").get()
+                .addOnSuccessListener {
+                    Log.d(Constans.TAG, "Got value ${it.value}")
 
-        // добавляем в остатках
-        //пытаюсь получить значение остатка по этому товару
-        database.child(Constans.KEY_DB_PRODUCTS).child(clickedId.value.toString()).child("quantity").get()
-            .addOnSuccessListener {
-                Log.d(Constans.TAG, "Got value ${it.value}")
+                    val value: Int = Integer.valueOf(it.value.toString())
+                    database.child(Constans.KEY_DB_PRODUCTS).child(clickedId.value.toString())
+                        .child("quantity").setValue(value + 1)
 
-                val value: Int = Integer.valueOf(it.value.toString())
-                database.child(Constans.KEY_DB_PRODUCTS).child(clickedId.value.toString())
-                    .child("quantity").setValue(value + 1)
-
-            }.addOnFailureListener {
-            Log.d(Constans.TAG, "Error getting data", it)
+                }.addOnFailureListener {
+                    Log.d(Constans.TAG, "Error getting data", it)
+                }
+            //убираем с продаж
+            //получаем текущее значение продаж
+            val quantity = _sales.value!!.find { it.id==_clickedId.value }!!.quantity
+            //где month - это выбранная текущая выборка месяца с БД
+            database.child(Constans.KEY_DB_SALES).child(month).child(clickedId.value.toString()).child("quantity").setValue(quantity-1)
         }
-         //убираем с продаж
-        //получаем текущее значение продаж
-        val quantity = _sales.value!!.find { it.id==_clickedId.value }!!.quantity
-        //где month - это выбранная текущая выборка месяца с БД
-        database.child(Constans.KEY_DB_SALES).child(month).child(clickedId.value.toString()).child("quantity").setValue(quantity-1)
+
 
     }
 }
--- app/src/main/java/com/chkan/warehouse_store/ui/warehouse/WarehouseViewModel.kt
+++ app/src/main/java/com/chkan/warehouse_store/ui/warehouse/WarehouseViewModel.kt
@@ -9,6 +9,10 @@ import com.chkan.warehouse_store.utils.Constans
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import org.joda.time.LocalDate
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.format.DateTimeFormatter
@@ -24,14 +28,15 @@ class WarehouseViewModel : ViewModel() {
 
     //для хранения списка товаров
     private val _products = MutableLiveData<List<Product>>()
-    val products : LiveData<List<Product>> = _products
+    val products: LiveData<List<Product>> = _products
     private val db: FirebaseDatabase = Firebase.database
     private val database: DatabaseReference = db.getReference(Constans.KEY_DB_PRODUCTS)
     private val database_sales: DatabaseReference = db.getReference(Constans.KEY_DB_SALES)
+
     //для передачи состояния
     private val _clickedId = MutableLiveData<Int>()
     val clickedId: LiveData<Int> = _clickedId
-    var sorted: List<Product>?=null
+    var sorted: List<Product>? = null
 
     init {
         getProducts()
@@ -43,14 +48,14 @@ class WarehouseViewModel : ViewModel() {
         database.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 Log.d(Constans.TAG, "WarehouseViewModel -> ValueEventListener")
-                val list : MutableList<Product> = mutableListOf()
+                val list: MutableList<Product> = mutableListOf()
                 // Здесь получаем список "детей" и проходимся по ним в цикле
                 for (data in dataSnapshot.children) {
                     var product = data.getValue(Product::class.java)
                     list.add(product as Product)
                 }
                 //отфильтровываем с 0 остатком и сортируем по названию суммок
-                sorted = list.filter { it.quantity!=0 }.sortedBy { it.name }
+                sorted = list.filter { it.quantity != 0 }.sortedBy { it.name }
                 _products.value = sorted
             }
 
@@ -62,36 +67,56 @@ class WarehouseViewModel : ViewModel() {
         })
     }
 
-    fun onProductClicked(id:Int){
+    fun onProductClicked(id: Int) {
         _clickedId.value = id
     }
 
-    fun onSold(){
-        val clickedId = _clickedId.value
-        val product = sorted!!.find { it.id==_clickedId.value }!!
-        if(sorted!=null){
-        //находим и уменьшаем остаток товара в БД
-            database.child(clickedId.toString()).child("quantity").setValue(product.quantity-1)
-        }
-        //записываем продажу в БД
-        val fmt: DateTimeFormatter = DateTimeFormat.forPattern("MMMMyy")
-        val month = fmt.print(LocalDate.now())// формате августа21
+    fun onSold() {
 
-        //пытаюсь получить значение продаж по этому товару, если их нет - получаю null
-        database_sales.child(month).child(clickedId.toString()).child("quantity").get().addOnSuccessListener {
-            Log.d(Constans.TAG, "Got value ${it.value}")
-            if(it.value==null){
-                //если значения нет - записываем обьект с 1 продажей
-                database_sales.child(month).child(clickedId.toString()).setValue(Product(clickedId!!,product.name,product.imageUrl,product.category,1,month))
-            } else {
-                val value:Int = Integer.valueOf(it.value.toString())
-                //если значение уже есть - увеличиваем его и обновляем
-                database_sales.child(month).child(clickedId.toString()).child("quantity").setValue(value+1)
+        //создаем неподвязанный скоуп чтобы корутина доработала и после закрытия фрагмента
+        //выбираем Dispatchers.IO потому что запросы в сеть
+
+        val scope = CoroutineScope(Job() + Dispatchers.IO)
+        scope.launch {
+
+            val clickedId = _clickedId.value
+            val product = sorted!!.find { it.id == _clickedId.value }!!
+            if (sorted != null) {
+                //находим и уменьшаем остаток товара в БД
+                database.child(clickedId.toString()).child("quantity")
+                    .setValue(product.quantity - 1)
+            }
+            //записываем продажу в БД
+            val fmt: DateTimeFormatter = DateTimeFormat.forPattern("MMMMyy")
+            val month = fmt.print(LocalDate.now())// формате августа21
+
+            //пытаюсь получить значение продаж по этому товару, если их нет - получаю null
+            database_sales.child(month).child(clickedId.toString()).child("quantity").get()
+                .addOnSuccessListener {
+                    Log.d(Constans.TAG, "Got value ${it.value}")
+                    if (it.value == null) {
+                        //если значения нет - записываем обьект с 1 продажей
+                        database_sales.child(month).child(clickedId.toString()).setValue(
+                            Product(
+                                clickedId!!,
+                                product.name,
+                                product.imageUrl,
+                                product.category,
+                                1,
+                                month
+                            )
+                        )
+                    } else {
+                        val value: Int = Integer.valueOf(it.value.toString())
+                        //если значение уже есть - увеличиваем его и обновляем
+                        database_sales.child(month).child(clickedId.toString()).child("quantity")
+                            .setValue(value + 1)
+                    }
+
+                }.addOnFailureListener {
+                Log.d(Constans.TAG, "Error getting data", it)
             }
 
-        }.addOnFailureListener{
-            Log.d(Constans.TAG, "Error getting data", it)
         }
-
     }
 }
